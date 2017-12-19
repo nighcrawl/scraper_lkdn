40951124b6b73c90157fa38b92397a9ee846d1bd
Ange Chierchia <ange.chierchia@gmail.com>
2017-12-19 10:41:46 +0100

    Ajoute le choix aléatoire d'un compte pour la connexion

--- scraper.py
+++ scraper.py
@@ -1,19 +1,36 @@
 # coding: utf8
+import os
+import sys
 from splinter import Browser
+from random import randint
+
+
+lkdn_profiles = [
+    {'login': 'michel@ibakus.com', 'pwd': 'KibogO*8011'},
+    {'login': 'dev3@ibakus.com', 'pwd': 'KibogO*8011'},
+    {'login': 'comm.manager@ibakus.com', 'pwd': 'KibogO*8011'}
+]
 
 url = "https://www.linkedin.com"
 browser = Browser('firefox')
 browser.visit(url)
-browser.fill('session_key', 'michel@ibakus.com')
-browser.fill('session_password', 'KibogO*8011')
+
+rand_profile = lkdn_profiles[randint(0,len(lkdn_profiles) - 1)]
+
+print "Connexion avec le compte '" + rand_profile['login'] + "'"
+
+browser.fill('session_key', rand_profile['login'])
+browser.fill('session_password', rand_profile['pwd'])
 loginSubmit = browser.find_by_id('login-submit')
 loginSubmit.click()
 
 keywords = 'expert+comptable'
 
+print "Recherche de(s) mot(s) clé(s) '" + keywords + "'"
+
 browser.visit('https://www.linkedin.com/search/results/index/?keywords=' + keywords + '&origin=GLOBAL_SEARCH_HEADER')
 
-#window.scrollTo(0, document.body.scrollHeight)
+# Force le scroll vers le bas pour éviter de récupérer des résultats vides lorsqu'ils ne sont pas dans le viewport
 browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
 results = browser.find_by_css('.search-results__primary-cluster ul.results-list li.search-result')
