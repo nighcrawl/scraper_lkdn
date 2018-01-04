241769828df3007489f20a62ab73f341874f6467
Ange Chierchia <ange.chierchia@gmail.com>
2018-01-04 11:28:57 +0100

    Modifie la fonction getPageList() pour qu'elle soit récursive

--- scraper.py
+++ scraper.py
@@ -1,8 +1,11 @@
 # coding: utf8
 import os
 import sys
+import time
 from splinter import Browser
 from random import randint
+
+browser = Browser('firefox')
 
 def main():
     lkdn_profiles = [
@@ -12,7 +15,6 @@ def main():
     ]
 
     url = "https://www.linkedin.com"
-    browser = Browser('firefox')
     browser.visit(url)
 
     rand_profile = lkdn_profiles[randint(0,len(lkdn_profiles) - 1)]
@@ -32,20 +34,16 @@ def main():
     print "Recherche de(s) mot(s) clé(s) '" + keywords + "'"
 
     browser.visit('https://www.linkedin.com/search/results/people/?facetGeoRegion=["lu%3A0"]&facetNetwork=%5B%22S%22%2C%22O%22%5D&keywords=' + keywords + '&origin=FACETED_SEARCH')
-    notLastPage = True
-    while notLastPage:
-        getPageList(browser)
-        if browser.is_element_present_by_css('button.next'):
-            browser.find_by_css('button.next').click();
-        else:
-            notLastPage = False
-
-# Fonction permettant d'afficher la liste des personnes de la page
-def getPageList(browser):
     # Force le scroll vers le bas pour éviter de récupérer des résultats vides lorsqu'ils ne sont pas dans le viewport
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
     browser.execute_script("window.scrollTo(0, 0);")
 
+    getPageList()
+
+# Fonction permettant d'afficher la liste des personnes de la page
+def getPageList():
+    time.sleep(10)
     # Récupère la liste de résultats
     results = browser.find_by_css('.search-results__primary-cluster ul.results-list li.search-result')
 
@@ -65,5 +63,15 @@ def getPageList(browser):
             'action': action.html.encode('utf8','xmlcharrefreplace').strip() if len(action) > 0 else None
         }
 
+    if browser.is_element_present_by_css('button.next'):
+        nextButton = browser.find_by_css('button.next')
+        nextButton.click()
+        
+        # Force le scroll vers le bas pour éviter de récupérer des résultats vides lorsqu'ils ne sont pas dans le viewport
+        browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(5)
+        browser.execute_script("window.scrollTo(0, 0);")
+
+        getPageList()
 
 main()
