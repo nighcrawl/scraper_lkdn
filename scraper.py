430487dcaf1df71526200feb76b6eb80373e9235
Ange Chierchia <ange.chierchia@gmail.com>
2018-01-15 12:00:14 +0100

    Corrige un bug d'encodage lors de l'envoi du message

--- scraper.py
+++ scraper.py
@@ -9,8 +9,7 @@ browser = Browser('firefox')
 
 # Fonction permettant de construire le message à envoyer
 def setMessage(item):
-    message = "Bonjour " + str(item['name']) + ",\r\nJ'aimerai vous parler d'un outil qui je pense pourrait vous être utile"
-    "Bonjour {FirstName},\nJ'aimerai beaucoup faire partie de votre réseau sur LinkedIn.\r\nNous développons des solutions technologiques anti-blanchiment à destination de votre métier et cela pourrait potentiellement vous intéresser dans le futur.\r\n\r\nBien à vous,\r\n\r\nMichel"
+    message = u"Bonjour " + item['name'].decode('utf-8') + u",\r\nJ'aimerai beaucoup faire partie de votre réseau sur LinkedIn.\r\nNous développons des solutions technologiques anti-blanchiment à destination de votre métier et cela pourrait potentiellement vous intéresser dans le futur.\r\n\r\nBien à vous,\r\n\r\nMichel"
     return message
 
 # Fonction permettant d'afficher la liste des personnes de la page
@@ -41,15 +40,17 @@ def getPageList(stop):
             action.click()
 
             # On clique sur le bouton permettant d'écrire un message à envoyer en plus de la demande de connexion
-            addNote = browser.find_by_css('div.send-invite__actions button.button-secondary-large')
+            addNote = browser.find_by_css('div.send-invite__actions button.button-secondary-large.mr1')
+            print "Click sur Ajouter une note"
             addNote.click()
 
             # Rempli le champ de message avant d'envoyer la demande de connexion
             message = setMessage(item)
             browser.fill('message', message)
-            sendInvite = browser.find_by_css('div.send-invite__actions button.button-primary-large')
+            sendInvite = browser.find_by_css('div.send-invite__actions button.button-primary-large.ml1')
+            print "Click sur Terminé"
             sendInvite.click()
-
+        print "----"
     # Si un lien permettant de passer à la page de résultats suivants et présent, on clique dessus puis on appelle getPageList()
     if browser.is_element_present_by_css('button.next'):
         nextButton = browser.find_by_css('button.next')
@@ -61,7 +62,7 @@ def getPageList(stop):
         browser.execute_script("window.scrollTo(0, 0);")
 
         if (stop == False):
-            getPageList()
+            getPageList(stop)
 
 # Fonction principale
 def main():
@@ -111,13 +112,13 @@ def main():
     print "Recherche de(s) mot(s) clé(s) '" + keywords + "'"
     print stop
     print page
-    sys.exit()
+    #sys.exit()
 
     # filtres = relation : 2e, 3e et +; lieux : belgique; secteurs : Comptabilité, Services financiers; langue : Français
-    filters  = '?facetGeoRegion=%5B"be%3A0"%5D&facetIndustry=%5B"47"%2C"43"%5D&facetNetwork=%5B"S"%2C"O"%5D&facetProfileLanguage=%5B"fr"%5D&keywords=' + keywords + '&origin=FACETED_SEARCH'
+    #filters  = '?facetGeoRegion=%5B"be%3A0"%5D&facetIndustry=%5B"47"%2C"43"%5D&facetNetwork=%5B"S"%2C"O"%5D&facetProfileLanguage=%5B"fr"%5D&keywords=' + keywords + '&origin=FACETED_SEARCH'
 
     # filtres = relation : 2e, 3e et +; lieux: luxembourg;
-    # filters = '?facetGeoRegion=["lu%3A0"]&facetNetwork=%5B%22S%22%2C%22O%22%5D&keywords=' + keywords + '&origin=FACETED_SEARCH'
+    filters = '?facetGeoRegion=["lu%3A0"]&facetNetwork=%5B%22S%22%2C%22O%22%5D&keywords=' + keywords + '&origin=FACETED_SEARCH'
 
     browser.visit('https://www.linkedin.com/search/results/people/' + filters)
     # Force le scroll vers le bas pour éviter de récupérer des résultats vides lorsqu'ils ne sont pas dans le viewport
@@ -125,6 +126,6 @@ def main():
     time.sleep(1)
     browser.execute_script("window.scrollTo(0, 0);")
 
-    getPageList()
+    getPageList(stop)
 
 main()
